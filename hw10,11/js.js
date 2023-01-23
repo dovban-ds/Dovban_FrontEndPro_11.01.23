0fe0b6414d9b8eaca1d9743368c9f2733f41dabe
Danylo Dovban <epumbar@gmail.com>
2023-01-23 23:02:38 +0200

    Changes

--- hw10,11/js.js
+++ hw10,11/js.js
@@ -1,86 +1,86 @@
 // Homework 10
 // Вивести на сторінку в один рядок через кому числа від 10 до 20.
-let figures = "";
-for (let i = 10; i <= 20; i++) {
-  //   figures += i + ", ";
-  figures += i === 20 ? i : i + ", ";
-  alert(figures.trim());
-}
+// let figures = "";
+// for (let i = 10; i <= 20; i++) {
+//   //   figures += i + ", ";
+//   figures += i === 20 ? i : i + ", ";
+//   alert(figures.trim());
+// }
 
 // Вивести квадрати чисел від 10 до 20.
-for (let i = 10; i <= 20; i++) {
-  alert(i ** 2);
-}
+// for (let i = 10; i <= 20; i++) {
+//   alert(i ** 2);
+// }
 
 // Вивести таблицю множення на 7.
-let sumSeven = "";
-for (let i = 1; i <= 10; i++) {
-  const result = i * 7;
-  sumSeven += " ".repeat(3) + result;
-  alert(sumSeven.trim());
-}
+// let sumSeven = "";
+// for (let i = 1; i <= 10; i++) {
+//   const result = i * 7;
+//   sumSeven += " ".repeat(3) + result;
+//   alert(sumSeven.trim());
+// }
 
 // Знайти суму всіх цілих чисел від 1 до 15.
-let sum = 0;
-for (let i = 1; i <= 15; i++) {
-  alert((sum += i));
-}
+// let sum = 0;
+// for (let i = 1; i <= 15; i++) {
+//   alert((sum += i));
+// }
 
 // Знайти добуток усіх цілих чисел від 15 до 35.
-let sumMult = 1;
-for (let i = 15; i <= 35; i++) {
-  alert((sumMult *= i));
-}
+// let sumMult = 1;
+// for (let i = 15; i <= 35; i++) {
+//   alert((sumMult *= i));
+// }
 
 // Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
-let sumAvvrg = 0;
-for (let i = 1; i <= 501; i++) {
-  i === 501 ? alert(sumAvvrg / 500) : alert((sumAvvrg += i));
-} // it looks very shitty i am not sure :D
+// let sumAvvrg = 0;
+// for (let i = 1; i <= 501; i++) {
+//   i === 501 ? alert(sumAvvrg / 500) : alert((sumAvvrg += i));
+// } // it looks very shitty i am not sure :D
 
 // Вивести суму лише парних чисел в діапазоні від 30 до 80.
-let sumEven = 0;
-for (let i = 30; i <= 80; i++) {
-  if (i % 2 !== 0) continue;
-  alert((sumEven += i));
-}
+// let sumEven = 0;
+// for (let i = 30; i <= 80; i++) {
+//   if (i % 2 !== 0) continue;
+//   alert((sumEven += i));
+// }
 
 // Вивести всі числа в діапазоні від 100 до 200 кратні 3.
-for (let i = 100; i <= 200; i++) {
-  if (i % 3 !== 0) continue;
-  alert(i);
-}
+// for (let i = 100; i <= 200; i++) {
+//   if (i % 3 !== 0) continue;
+//   alert(i);
+// }
 
 // Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
-let naturalNumber = 100;
-for (let i = 1; i <= 100; i++) {
-  if (naturalNumber % i !== 0) continue;
-  alert(i);
-}
+// let naturalNumber = 100;
+// for (let i = 1; i <= 100; i++) {
+//   if (naturalNumber % i !== 0) continue;
+//   alert(i);
+// }
 
 // Визначити кількість його парних дільників.
-let naturalNextNumber = 100;
-let pairNaturalNumber = 0;
-for (let i = 1; i <= 100; i++) {
-  if (naturalNextNumber % i !== 0) continue;
-  if (i % 2 == 0) alert(pairNaturalNumber + i);
-}
+// let naturalNextNumber = 100;
+// let pairNaturalNumber = 0;
+// for (let i = 1; i <= 100; i++) {
+//   if (naturalNextNumber % i !== 0) continue;
+//   if (i % 2 == 0) alert(pairNaturalNumber + i);
+// }
 
 // Знайти суму його парних дільників.
-let naturalNumberSecond = 100;
-let pairNaturalNumberSecond = 0;
-for (let i = 1; i <= 100; i++) {
-  if (naturalNumberSecond % i !== 0) continue;
-  if (i % 2 == 0) alert((pairNaturalNumberSecond += i));
-}
+// let naturalNumberSecond = 100;
+// let pairNaturalNumberSecond = 0;
+// for (let i = 1; i <= 100; i++) {
+//   if (naturalNumberSecond % i !== 0) continue;
+//   if (i % 2 == 0) alert((pairNaturalNumberSecond += i));
+// }
 
 // Надрукувати повну таблицю множення від 1 до 10.
-for (let i = 1; i <= 10; i++) {
-  for (let j = 1; j <= 10; j++) {
-    const result = i * j;
-    alert(`${i} * ${j} = ${result}`);
-  }
-}
+// for (let i = 1; i <= 10; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     const result = i * j;
+//     alert(`${i} * ${j} = ${result}`);
+//   }
+// }
 
 // Homework 11
 // Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
@@ -88,8 +88,8 @@ let someFigures = "";
 for (let i = 20; i <= 30; i += 0.5) {
   const result = String(i);
   someFigures += " ".repeat(2) + result;
-  alert(someFigures.trim());
 }
+console.log(someFigures.trim());
 
 // Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
 let dollar = 27;
@@ -97,18 +97,21 @@ let sumDol = " ";
 for (let i = 10; i <= 100; i += 10) {
   let result = dollar * i + "";
   sumDol += " ".repeat(3) + result;
-  alert(sumDol.trim());
+  // if (i == 100) console.log(sumDol.trim());
 }
+console.log(sumDol.trim());
 
 // Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
 let usersFigure = +prompt("Your number here");
 let figuresSum = " ";
+let supRes;
 for (let i = 1; i <= 100; i++) {
   const result = i;
   if (usersFigure > i ** 2) {
-    alert((figuresSum += " ".repeat(3) + result).trim());
+    supRes = (figuresSum += " ".repeat(3) + result).trim();
   }
 }
+console.log(supRes.trim());
 
 // Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
 const usersNextFigure = prompt("Enter a positive number");
