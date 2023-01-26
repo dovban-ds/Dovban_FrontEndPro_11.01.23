c3389c9d2a80513a1e65d70e5cabc0fd9fcab7f7
Danylo Dovban <epumbar@gmail.com>
2023-01-26 23:29:06 +0200

    Homework 15

---
+++ hw15/js.js
@@ -0,0 +1,7 @@
+const vegetablesArray = ["potato", "tomato", "cucumber", "carrot", "cabbage"];
+
+vegetablesArray.forEach((vegetable) => console.log(vegetable));
+
+for (let vegetable of vegetablesArray) {
+  console.log(vegetable);
+}
