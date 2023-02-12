7b62308567746612bb751343cec491c2c2542cb3
Danylo Dovban <epumbar@gmail.com>
2023-02-12 21:21:43 +0200

    Homework 23

---
+++ hw23/script.js
@@ -0,0 +1,48 @@
+// Створити ланцюжок прототипів машина => певна марка машини.
+// У підсумкового інстансу повинні бути функції машини типу їхати і гальмувати з прототипу так і функції пов'язані з маркою машини типу вертикального відкриття дверей Lamborghini.
+
+function Car(model) {
+  this.model = model;
+}
+
+const features = {
+  accelerate: function () {
+    console.log("accelerating");
+  },
+  breake: function () {
+    console.log("breaking");
+  },
+  park: function () {
+    console.log("parking");
+  },
+};
+
+Car.prototype = features;
+
+const bmw = new Car("BMW");
+const lamborghini = new Car("Lamborghini");
+const mitsubishi = new Car("Mitsubishi");
+const toyota = new Car("Toyota");
+
+const bmwFeature = function () {
+  console.log("Turn signals");
+};
+const lamboFeature = function () {
+  console.log("Butterfly doors");
+};
+const mitsFeature = function () {
+  console.log("Rally-sprint");
+};
+const toyotaFeature = function () {
+  console.log("JDM culture");
+};
+
+bmw.specTurn = bmwFeature;
+lamborghini.specDoors = bmwFeature;
+mitsubishi.specRally = bmwFeature;
+toyota.specCulture = bmwFeature;
+
+console.log(bmw);
+console.log(mitsubishi);
+console.log(toyota);
+console.log(lamborghini);
