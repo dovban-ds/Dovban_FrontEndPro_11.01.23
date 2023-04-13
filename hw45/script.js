caa13f9cfd410a6c8bb614e8e53c66e8651e1bb9
Danylo Dovban <epumbar@gmail.com>
2023-04-13 12:12:00 +0300

    Some upd

--- hw45/script.js
+++ hw45/script.js
@@ -2,14 +2,13 @@ const weatherCase = document.querySelector(".weather-case");
 
 function doForecast(city) {
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
   )
     .then((response) => response.json())
     .then((info) => {
-      console.log(info);
       const HTML = `
         <div class='div-style'>
-        <img src="http://openweathermap.org/img/w/${info.weather[0].icon}.png" alt="weather picture" style ="align-self: center; margin: 8px 0">
+        <img src="https://openweathermap.org/img/w/${info.weather[0].icon}.png" alt="weather picture" style ="align-self: center; margin: 8px 0">
         <p>City: ${info.name}</p>
         <p style = 'margin: 8px 0'>Temperature: ${info.main.temp} ℃</p>
         <p>Pressure: ${info.main.pressure} mm</p>
